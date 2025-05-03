722428bf1f1e23b7bd888f03fca64594de563e73
angeleVG <a.vangroesen@outlook.com>
2025-05-03 16:35:48 +0200

    create footer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,17 @@
 import NavBar from "../components/NavBar";
-import Offer from "../components/Offer"
+import Header from "../components/Header"
 import ProductList from "../components/ProductList";
+import Footer from "../components/Footer"
 
 export default function HomePage () {
     return (
         <>
+        <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
         <NavBar />
         <Header />
         <ProductList />
+        <Footer />
+        </div>
         </>
     )
 }
