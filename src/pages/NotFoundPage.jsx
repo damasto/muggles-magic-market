4143de5301ce1992749b6b6194bc81287af70331
Daniel Fischer <daniel.altavilla94@gmail.com>
2025-05-06 21:10:52 +0200

    create NotFoundPage

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+    return (
+        <>
+            <h1>We didn't find anything to show here. Was it written with invisible ink? Try "Aparecium" !</h1>
+        </>
+    )
+}
