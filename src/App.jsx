6efc02a8e11b1ed02721843b985788249090faa2
Daniel Fischer <daniel.altavilla94@gmail.com>
2025-05-06 20:45:49 +0200

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css'
-import HomePage from './pages/HomePage'
 import { Routes, Route } from 'react-router-dom'
+
+import CategoryPage from './pages/CategoryPage'
+import HomePage from './pages/HomePage'
+import ProductDetailPage from './pages/ProductDetailPage'
+import  ShoppingCart  from "./pages/ShoppingCart"
+import CheckoutPage from './pages/CheckoutPage'
 
 function App() {
 
@@ -8,6 +13,10 @@ function App() {
     <>
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
+      <Route path="/category" element={<CategoryPage />}></Route>
+      <Route path="/product-detail/:productId" element={<ProductDetailPage />}></Route>
+      <Route path="/shopping-cart" element={<ShoppingCart />}></Route>
+      <Route path="/checkout" element={<CheckoutPage />}></Route>
     </Routes>
     </>
   )
