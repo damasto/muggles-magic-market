923b5aa15a0469d53388b9d5521def2a23b69ff3
Daniel Fischer <daniel.altavilla94@gmail.com>
2025-05-06 19:34:37 +0200

    install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
 import HomePage from './pages/HomePage'
+import ProductDetailPage from "./pages/ProductDetailPage"
+import CategoryPage1 from "./pages/CategoryPage"
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-    <HomePage /> 
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/product-detail/:productId" element={ <ProductDetailPage />}></Route>
+      <Route path="/category/:category" element={<CategoryPage1/>}></Route>
+      
+
+    </Routes>
     </>
   )
 }
