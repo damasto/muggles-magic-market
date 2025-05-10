2a3685f21a584795cde061b2a6201b6cac2dd8a2
angeleVG <a.vangroesen@outlook.com>
2025-05-10 16:51:08 +0200

    fix typo in title

--- src/pages/AllProductsPage.jsx
+++ src/pages/AllProductsPage.jsx
@@ -48,7 +48,7 @@ export default function AllProductsPage() {
             letterSpacing: "2px",
           }}
         >
-          Alle Producten
+          All Products
         </Typography>
       </Box>
       <Box
