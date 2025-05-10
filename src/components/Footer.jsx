9ac5f9b99e61f4215437368290b2308c164bf939
Daniel Fischer <daniel.altavilla94@gmail.com>
2025-05-10 12:01:33 +0200

    recover productList

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,20 +5,22 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 const Footer = () => {
   return (
     <Box component="footer" sx={{ bgcolor: 'grey.100', py: 2, mt: 'auto', marginTop: 4 }}>
-      <Link
-        style={{textDecoration: "none"}}
-        href="https://github.com/damasto/muggles-magic-market"
-        target="_blank"
-        rel="noopener"
-        color="inherit"
-      >
-        <Stack direction="column" justifyContent="center" alignItems="center" spacing={1}>
+      <Stack direction="column" justifyContent="center" alignItems="center" spacing={1}>
+        <Link
+          id='footer-git'
+          style={{ textDecoration: "none" }}
+          href="https://github.com/damasto/muggles-magic-market"
+          target="_blank"
+          rel="noopener"
+          color="inherit"
+        >
+           <GitHubIcon fontSize="small" />
           <Typography variant="body2" color="textSecondary">
             Developed by Daniel and Angèle
           </Typography>
-          <GitHubIcon fontSize="small" />
-        </Stack>
-      </Link>
+         
+        </Link>
+      </Stack>
     </Box>
   );
 };
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -83,25 +83,51 @@ export default function ProductList() {
         >
           {displayedProducts.map((product) => (
             <Grid item xs={12} sm={6} md={4} key={product.id}>
-
-              <Link className="link" to={`product-details/${product.id}`}>
+              <Link className="link" to={`/product-details/${product.id}`}>
                 <Card
                   sx={{
-                    width: '100%',
-                    height: '100%',
-                    objectFit: 'cover',
+                    width: 250,
+                    height: 350,
+                    display: "flex",
+                    flexDirection: "column",
                   }}
-                  image={`${imageAPI}${product.image}`}
-                  alt={product.title}
                 >
-                  <CardContent sx={{ flexGrow: 1, paddingBottom: 0, display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }}>
-                    <Typography variant="h6" sx={{ fontWeight: 'bold', marginBottom: 0.25 }}>
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                    }}
+                    image={`${imageAPI}${product.image}`}
+                    alt={product.title}
+                  />
+                  <CardContent
+                    sx={{
+                      flexGrow: 1,
+                      paddingBottom: 0,
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "flex-start",
+                    }}
+                  >
+                    <Typography
+                      variant="h6"
+                      sx={{ fontWeight: "bold", marginBottom: 0.25 }}
+                    >
                       {product.title}
                     </Typography>
                   </CardContent>
-                  <CardActions sx={{ justifyContent: 'space-between', padding: '4 16px 8px 16px' }}>
+                  <CardActions
+                    sx={{
+                      justifyContent: "space-between",
+                      padding: "4 16px 8px 16px",
+                    }}
+                  >
                     <Button size="small">Details</Button>
-                    <Button size="small">Add to Cart</Button>
+                    <Button size="small">
+                      <ShoppingCartIcon />
+                    </Button>
                   </CardActions>
                 </Card>
               </Link>
