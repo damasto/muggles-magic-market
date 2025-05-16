bd13593def83873bfd6943b29dfaeda418bc472d
angeleVG <a.vangroesen@outlook.com>
2025-05-16 10:35:48 +0200

    add total price in shoppingcart

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -15,6 +15,11 @@ export default function ShoppingCart() {
     );
   }
 
+   const totalPrice = shoppingCart.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
+
   return (
     <Container maxWidth="md">
       <Box sx={{ mt: 4 }}>
@@ -25,23 +30,39 @@ export default function ShoppingCart() {
           <Typography>Your cart is empty.</Typography>
         ) : (
           <>
-            <List>
+            <List disablePadding>
              {shoppingCart.map((item) => (
-  <div key={item.id}>
-    <ListItem>
+   <React.Fragment key={item.id}>
+    <ListItem
+       secondaryAction={
+         <Button
+                      variant="outlined"
+                      color="error"
+                      size="small"
+                        onClick={() => removeItem(item.id)}
+                    >
+                       Remove
+                    </Button>
+                  }
+                  sx={{ px: 0 }}
+                >
       <ListItemText
         primary={item.product ? item.product.title : item.title}
-        secondary={`€${item.price} - Quantity: ${item.quantity}`}
+        secondary={`€${item.price} - quantity: ${item.quantity}`}
       />
-      <Button onClick={() => removeItem(item.id)}>Remove</Button>
     </ListItem>
-    <Divider />
-  </div>
-))}
-            </List>
-          </>
-        )}
-      </Box>
+    <Divider component="li" />
+      </React.Fragment>
+            ))}
+          </List>
+      <Box sx={{ mt: 2, display: 'flex', justifyContent: 'flex-end' }}>
+        <Typography variant="h6" fontWeight="bold">
+            Total: €{totalPrice.toFixed(2)}
+        </Typography>
+        </Box>
+        </>
+     )}
+        </Box>
     </Container>
   );
 }
